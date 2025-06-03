3b2a62e783278d8ccf9f9cf73f7cd57aceb9c6b2
sarang-m <sarangsrg10@gmail.com>
2025-06-03 16:52:55 +0530

    updated postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
+export default {
   plugins: {
     tailwindcss: {},
   },
 };
 
-export default config;
+//export default config;
